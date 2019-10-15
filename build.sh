6ceef8010da4605d7a19cf14c0160efb6d015fb5
Lars Brinkhoff <lars@nocrew.org>
2019-10-15 18:00:45 +0200

    Updated RUG.
    Load symbol table off paper tape.  Add $^N command.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ../its/tools/dasm/palx -A < files/sits/sits.bin > sits.abs
-../its/tools/dasm/palx -A < files/rug/ar.bin > rug.abs
+../its/tools/dasm/palx -A < files/rug/ar633.bin > rug.abs
 ../its/tools/dasm/palx -A < files/sits/salv.bin > salv.abs
 
 ../its/tools/simh/BIN/pdp11 salv.simh
